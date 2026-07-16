b8b6aa3f1d173007a7c937f1668efd87f38927b7
itsFakhrulIslam <fakhrulishere@gmail.com>
2026-07-16 12:00:29 +0600

    database connect error

--- module54/crudServer/index.js
+++ module54/crudServer/index.js
@@ -1,16 +1,45 @@
 const express = require("express");
-const cors = require('cors');
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 2030;
 
 // middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+
+// mongoDb connector here
+// crudAppDb
+// 9EH6NfUiosqYX5c0
+const uri =
+  "mongodb+srv://crudAppDb:9EH6NfUiosqYX5c0@crudapp.rsibkyv.mongodb.net/?appName=crudApp";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 app.get("/", (req, res) => {
   res.send("hello! crud server is running...");
 });
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log("server is going on", port);
