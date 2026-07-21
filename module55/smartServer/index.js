6077b65793a340a6fedb23bc04b0fe2733c48c82
itsFakhrulIslam <fakhrulishere@gmail.com>
2026-07-21 22:31:39 +0600

    post, delet apis created with successfully tested in thunder client

--- module55/smartServer/index.js
+++ module55/smartServer/index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const applicatioin = express();
 const port = process.env.PORT || 2040;
 
 // NOTE: Using standard mongodb:// URI to avoid SRV DNS issues on this network.
 // For production/Atlas best practice, can switch to mongodb+srv:// when DNS is stable.
-const uri = "mongodb://smartDealsDb:BAo57p4QHpKCQFfM@ac-gxh4w60-shard-00-00.rsibkyv.mongodb.net:27017,ac-gxh4w60-shard-00-01.rsibkyv.mongodb.net:27017,ac-gxh4w60-shard-00-02.rsibkyv.mongodb.net:27017/?ssl=true&replicaSet=atlas-vflvht-shard-0&authSource=admin&appName=crudApp";
+const uri =
+  "mongodb://smartDealsDb:BAo57p4QHpKCQFfM@ac-gxh4w60-shard-00-00.rsibkyv.mongodb.net:27017,ac-gxh4w60-shard-00-01.rsibkyv.mongodb.net:27017,ac-gxh4w60-shard-00-02.rsibkyv.mongodb.net:27017/?ssl=true&replicaSet=atlas-vflvht-shard-0&authSource=admin&appName=crudApp";
 
 // middleware
 applicatioin.use(cors());
@@ -15,6 +16,10 @@ applicatioin.use(express.json());
 
 applicatioin.get("/", (req, res) => {
   res.send("hello smart developers");
+});
+
+applicatioin.get("/products", (req, res) => {
+  res.send();
 });
 
 const client = new MongoClient(uri, {
@@ -28,6 +33,28 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
+
+    // create db with collection area
+    const smartDB = client.db("smartDatabase");
+    const productsCollections = smartDB.collection("products");
+
+    // database/crud operation apis area
+    applicatioin.post("/products", async (req, res) => {
+      // new products create
+      const newProduct = req.body;
+      const createdResult = await productsCollections.insertOne(newProduct);
+      res.send(createdResult);
+    });
+
+    applicatioin.delete("/products/:id", (req, res) => {
+      // product delete
+      const productId = req.params.id;
+      const query = { _id: new ObjectId(productId) };
+      const deleteResult = productsCollections.deleteOne(query);
+      res.send();
+    });
+
+    // database connect with logging area
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
